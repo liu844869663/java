32d570f5626c4aea33908b2352dc62c9bb5bf49c
刘景平 <844869663@qq.com>
2021-04-16 08:40:49 +0800

    Javassist 动态代理使用示例

---
+++ basic/src/main/java/com/fullmoon/study/proxy/JavassistProxy.java
@@ -0,0 +1,35 @@
+package com.fullmoon.study.proxy;
+
+import javassist.util.proxy.ProxyFactory;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author jingping.liu
+ * @date 2021-04-15
+ * @description Javassist 动态代理
+ */
+public class JavassistProxy {
+
+    public void say() {
+        System.out.println("hello, liujingping");
+    }
+
+    public static void main(String[] args) throws Exception {
+        ProxyFactory factory = new ProxyFactory();
+        // 指定代理类的父类
+        factory.setSuperclass(JavassistProxy.class);
+        // 设置方法过滤器，用于判断是否拦截方法
+        factory.setFilter((Method method) -> {
+            // 拦截 `say` 方法
+            return "say".equals(method.getName());
+        });
+        JavassistProxy proxy = (JavassistProxy) factory.create(new Class<?>[]{}, new Object[]{}, (o, method, method1, objects) -> {
+            System.out.println("前置处理");
+            Object result = method1.invoke(o, objects);
+            System.out.println("后置处理");
+            return result;
+        });
+        proxy.say();
+    }
+}
